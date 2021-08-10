31e7e73c417e68e0903b32de9143f82620cf8eee
Philip P Perez <pperez@slipsleeve.com>
2021-08-09 17:15:40 -0700

    Initial Commit

---
+++ ASGEphemeris/ASGEphemeris.h
@@ -0,0 +1,18 @@
+//
+//  ASGEphemeris.h
+//  ASGEphemeris
+//
+//  Created by Philip Perez on 8/9/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ASGEphemeris.
+FOUNDATION_EXPORT double ASGEphemerisVersionNumber;
+
+//! Project version string for ASGEphemeris.
+FOUNDATION_EXPORT const unsigned char ASGEphemerisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASGEphemeris/PublicHeader.h>
+
+
